9150925491bd62e50ad51a0fa4c87d5b476730cf
Guy Cherubin <ngchrbn@gmail.com>
2024-04-23 01:01:07 +0200

    Remove timeout and error throwing

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -105,7 +105,6 @@ export async function createInvoice(prevState: State, formData: FormData) {
   }
 
   export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
     try {
       await sql`DELETE FROM invoices WHERE id = ${id}`;
       revalidatePath('/dashboard/invoices');
